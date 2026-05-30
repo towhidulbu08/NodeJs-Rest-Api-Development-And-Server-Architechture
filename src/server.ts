4fc2d540eb988594bdda4f64a5bddaa4809ad4d8
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-05-31 03:54:00 +0600

    .env file created and loaded process.env.PORT

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
+import config from "./config";
+
 import { createServer, IncomingMessage, Server } from "http";
+
 import { routeHandler } from "./routes/route";
 
 const server: Server = createServer((req: IncomingMessage, res) => {
   routeHandler(req, res);
 });
-
-server.listen(5000, () => {
-  console.log("server is running on the port: 5000");
+// console.log("PORT =", process.env.PORT);
+server.listen(config.port, () => {
+  console.log("server is running on the port: 8000");
 });
